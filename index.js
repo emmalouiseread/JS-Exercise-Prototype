8a60c16be9bad370dffe3679927da9da35d6d23c
emmalouiseread <73863740+emmalouiseread@users.noreply.github.com>
2021-01-27 14:38:52 -0600

    fin

--- index.js
+++ index.js
@@ -39,8 +39,22 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  Person.prototype.eat = function(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+  }
+  console.log(edible);
+}
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
+  Person.prototype.toString = function(name, age) {
+    return `${this.name}, ${this.age}`;
   }
  
  
@@ -63,8 +77,20 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  Car.prototype.fill = function(gallons) {
+    this.gallons = gallons;
+    this.tank += this.gallons;
+  }
+  Car.prototype.drive = function(distance) {
+    this.distance = distance;
+    this.odometer += this.distance;
+    this.tank = distance / this.milesPerGallon;
   }
   
   
@@ -75,17 +101,22 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
  
+ function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age) // this is telling the child to inherit all the key value pairs from the parent
+  this.favoriteToy = favoriteToy;
+}
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+  } 
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
+    1. Window Binding. If none of the other rules apply 'this' defaults to the window - unless you are in strict mode in which case it will return undefined. We do not want to use window binding.
+    2. Implicit Binding is the most common rule - used in about 80% of use cases. When the function is invoked, look to the left of the dot, that's what 'this' referes to. It exists within a method.
+    3. Explicit binding means invoking the function - look to the left of the dot in the invocation. .call will immediately invoke the function
     4. 
   */
   
@@ -97,9 +128,9 @@ function foo(){
     return 'bar';
 }
 
-export default{
+ export default{
     foo,
     Person, 
     Car,
     Baby
-}
+} 
